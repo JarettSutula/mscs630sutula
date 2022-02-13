81e3fdac3ce314c9ec03c1141eb2ecf05a1189fa
JarettSutula <Jarett.Sutula1@marist.edu>
2022-02-13 16:33:41 -0500

    working lab4 code w/o docs

---
+++ labs/Driver_lab4.java
@@ -0,0 +1,20 @@
+/**
+ * file: Driver_lab4.java
+ * author: Jarett Sutula
+ * course: MSCS 630L
+ * assignment: lab 4 - Generating Secure Keys
+ *
+ * due date: February 20th, 2022
+ * version: 1.0
+ *
+ *
+ */
+import java.util.Scanner;
+
+public class Driver_lab4 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String keyHex = input.nextLine();
+    input.close();
+  }
+}
